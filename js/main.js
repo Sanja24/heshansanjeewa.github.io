936e37553c2b9ca4936cbe92615bb153cfcbae75
Heshan Sanjeewa <heshansanjeewa.hs@gmail.com>
2026-01-12 20:12:10 +0530

    Animation and Site Created

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-// Smooth scrolling for navigation
+// Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href'))
-            .scrollIntoView({ behavior: 'smooth' });
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
     });
 });
